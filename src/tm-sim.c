b1ce3434286a779986a30137258a4676e5583736
Maximilian Wagner <maximilian.wagner@smail.inf.h-brs.de>
2023-11-28 16:19:56 +0100

    Converted program to library

--- src/tm-sim.c
+++ src/tm-sim.c
@@ -1,41 +1,8 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include "util/dllist.h"
-
-
-// max size of a single state
-#define STATE_SIZE 8
-
-// max number of steps
-#define MAX_RULE_NUM 60
-
-// number of variables contained in a single step
-#define VARS_IN_RULE 5
-
-// max size of single entry in step
-#define MAX_RULE_SIZE 16
-
-
-// here are the defining features of the tm 
-struct turing {
-  short bands;
-  char  start  [STATE_SIZE];
-  char  accept [STATE_SIZE];
-  char  reject [STATE_SIZE];
-};
-
-
-struct state {
-  char  state [STATE_SIZE];
-  struct dnode*  band;
-  long  band_location;
-};
-
-
-struct turing tim = {0, {'\0'}, {'\0'}, {'\0'}};
-char steps[MAX_RULE_NUM][VARS_IN_RULE][MAX_RULE_SIZE] = {{{'\0'}}};
+#include "tm-sim.h"
 
 
 void clean_string(char input[], char io) {
@@ -97,7 +64,7 @@ void buf_reset(char arr[]) {
 }
 
 
-void parse_definition(char input[]) {
+void parse_definition(struct turing* turing, char input[]) {
   char buf[STATE_SIZE] = {'\0'}; // buffer
   short buf_index = 0; // tracks next char in buffer
   short sem_count = 0; // tracks count of semicolons
@@ -106,19 +73,19 @@ void parse_definition(char input[]) {
     if (input[i] == ';') {
       switch (sem_count) {
         case 0: // copy over amount of bands
-          tim.bands = buf_sum(buf);
+          turing -> bands = buf_sum(buf);
           break;
 
         case 1: // copy over starting state
-          memcpy(tim.start, buf, STATE_SIZE);
+          memcpy(turing -> start, buf, STATE_SIZE);
           break;
 
         case 2: // copy over accepting state
-          memcpy(tim.accept, buf, STATE_SIZE);
+          memcpy(turing -> accept, buf, STATE_SIZE);
           break;
 
         case 3: // copy over rejecting state
-          memcpy(tim.reject, buf, STATE_SIZE);
+          memcpy(turing -> reject, buf, STATE_SIZE);
           return;
       }
       
@@ -134,12 +101,13 @@ void parse_definition(char input[]) {
 }
 
 
-void parse_steps(char input[]) {
+void parse_steps(struct turing* turing, char input[]) {
   char buf[STATE_SIZE] = {'\0'}; // buffer
   short buf_index = 0; // tracks index of next char in buffer 
   short sem_count = 0; // tracks the number of semicolons counted
   short input_start_index = 0; // index of first char after definition
   short sym_count = 0; // counts ',' and '-' symbols
+  char ***steps = malloc(sizeof(char[MAX_RULE_NUM][VARS_IN_RULE][MAX_RULE_SIZE]));
 
   // get index where tm steps start
   do {
@@ -184,24 +152,30 @@ void parse_steps(char input[]) {
 
   // copy remaining step member over
   memcpy(steps[sem_count][sym_count++], buf, strlen(buf));
+
+  turing -> steps = steps;
 }
 
 
-// returns the index in steps array of the applicable step
-void run_tm(struct state* state) {
-  struct dnode* band = state->band;
+void run_tm(struct turing* turing) {
+  struct dnode* band = turing->state->band;
+  char *accept = turing->accept;
+  char *reject = turing->reject;
+  struct state* state = turing->state;
+  char ***steps = turing->steps;
+
   // while neither accepting or rejecting state are reached
-  while (strcmp(tim.accept, (*state).state) && strcmp(tim.reject, (*state).state)) {
+  while (strcmp(accept, state->state) && strcmp(reject, state->state)) {
     // go over every possible step and
     for (int i = 0; i < MAX_RULE_NUM; ++i) {
       // if satisfying below conditions, execute step
-      if (!strcmp(steps[i][0], (*state).state) && band->value == steps[i][1][0]) {
+      if (!strcmp(steps[i][0], state->state) && band->value == steps[i][1][0]) {
         // print the used step
         // printf("%s %s -> %s %s %s\n", steps[i][0], steps[i][1], steps[i][2], steps[i][3], steps[i][4]);
         band->value = steps[i][3][0];
 
-        memset((*state).state, '\0', STATE_SIZE);
-        strcpy((*state).state, steps[i][2]);
+        memset(state->state, '\0', STATE_SIZE);
+        strcpy(state->state, steps[i][2]);
 
         switch (steps[i][4][0]) {
           case '>':
@@ -230,145 +204,95 @@ void run_tm(struct state* state) {
 }
 
 
-// runs the simulator
-char* start_tm(char tm[], char input_band[]) {
-  if (tim.start == tim.accept) {
-    printf("do you actually want me to do something?\n");
-    return "";
+int run_tm_single_step(struct turing* turing) {
+  struct dnode* band = turing->state->band;
+  char *accept = turing->accept;
+  char *reject = turing->reject;
+  struct state* state = turing->state;
+  char ***steps = turing->steps;
+
+  if (strcmp(accept, state->state))
+    return TM_ACCEPT;
+
+  if (strcmp(reject, state->state))
+    return TM_REJECT;
+
+  // go over every possible step and
+  for (int i = 0; i < MAX_RULE_NUM; ++i) {
+    // if satisfying below conditions, execute step
+    if (!strcmp(steps[i][0], state->state) && band->value == steps[i][1][0]) {
+      // print the used step
+      // printf("%s %s -> %s %s %s\n", steps[i][0], steps[i][1], steps[i][2], steps[i][3], steps[i][4]);
+      band->value = steps[i][3][0];
+
+      memset(state->state, '\0', STATE_SIZE);
+      strcpy(state->state, steps[i][2]);
+
+      switch (steps[i][4][0]) {
+        case '>':
+          if (band->next == NULL) {
+            dlist_append(band, '_');
+          }
+          band = band->next;
+          break;
+        
+        case '<':
+          if (band->prev == NULL) {
+            dlist_prepend(band, '_');
+          }
+          band = band->prev;
+          break;
+        
+        case '-':
+          // no change
+          break;
+      }
+    }
+
+    break;
   }
 
-  // clean input and convert input to dlist
-  clean_input(tm);
-  clean_input(input_band);
-  struct dnode* band_list = dlist_from_string(input_band);
-
-  // parse defining features of tm
-  parse_definition(tm);
-  
-  // parse conversion steps of tm
-  parse_steps(tm);
-
-  struct state state = {{'\0'}, NULL, 0};
-
-  strcpy(state.state, tim.start);
-  state.band = band_list;
-  
-  run_tm(&state);
-
-  char* output = dlist_to_string(band_list);
-  
-  clean_output(output);
-  return output;
+  return TM_OKAY;
 }
 
 
-// void dll_test(char input[]) {
-//   struct dnode* start = malloc(sizeof(struct dnode));
-//
-//   dlist_append(start, 'b');
-//   dlist_append(start, 'd');
-//   
-//   start = dlist_prepend(start, 'a');
-//   
-//   dlist_insert(start, 'c', 2);
-//   
-//   printf("dlist_to_string: %s\n", dlist_to_string(start));
-//
-//   clean_input(input);
-//   struct dnode* list_from_string = dlist_from_string(input);
-//   printf("dlist_from_string_to_string: %s\n\n", dlist_to_string(list_from_string));
-// }
+struct turing* tm_create_machine(char tm[]) {
+  struct turing* turing = malloc(sizeof(struct turing));
 
+  // clean input and convert input to dlist
+  clean_input(tm);
 
-int main(int argc, char *argv[]) {
-  // tm that increments the input
-  char tm1[] =
-    "1; q0; q3; q4;"
-    "q0, 0 -> q0, 0, > ;"
-    "q0, 1 -> q0, 1, > ;"
-    "q0, _ -> q1, _, < ;"
-    "q1, 0 -> q2, 1, < ;"
-    "q1, 1 -> q1, 0, < ;"
-    "q1, _ -> q3, 1, - ;"
-    "q2, 1 -> q2, 1, < ;"
-    "q2, 0 -> q2, 0, < ;"
-    "q2, _ -> q3, _, >";
+  // parse input into tm
+  parse_definition(turing, tm);
+  parse_steps(turing, tm);
+
+  // create starting state
+  struct state* state = malloc(sizeof(struct state));
+  strcpy(state -> state, turing -> start);
+  turing -> state = state;
   
-  // tm that decrements the input
-  char tm2[] =
-    "1; q0; q3; q4;"
-    "q0, 0 -> q0, 0, >;"
-    "q0, 1 -> q6, 1, >;"
-    "q0, _ -> q4, _, -;"
-    "q6, 0 -> q6, 0, >;"
-    "q6, 1 -> q6, 1, >;"
-    "q6, _ -> q1, _, <;"
-    "q1, 0 -> q2, 1, <;"
-    "q1, 1 -> q5, 0, <;"
-    "q1, _ -> q4, _, -;"
-    "q2, 0 -> q2, 1, <;"
-    "q2, 1 -> q5, 0, <;"
-    "q5, 0 -> q5, 0, <;"
-    "q5, 1 -> q5, 1, <;"
-    "q5, _ -> q3, _, >";
-
-  // tm that doubles the input
-  char tm3[] =
-    "1; q0; q2; q3;"
-    "q0, 0 -> q0, 0, >;"
-    "q0, 1 -> q0, 1, >;"
-    "q0, _ -> q1, 0, <;"
-    "q1, 0 -> q1, 0, <;"
-    "q1, 1 -> q1, 1, <;"
-    "q1, _ -> q2, _, >";
-  
-  char* output;
-
-  char band1[] = "0110 1001 0101 1010 1111 0000 1111 0000";
-  char band2[] = "1001 0110 1010 0101 0110 1001 0101 1010";
-  char band3[] = "1111 0000 1111 0000 1001 0110 1010 0101";
+  return turing;
+}
 
 
-  //-----------------------------------------
-  // dll_test(band1);
+int tm_set_band(struct turing* turing, char input_band[]) {
+  clean_input(input_band);
 
-  //-----------------------------------------
-  printf("INCREMENT\n");
+  if (turing -> state != NULL) {
+    free(turing -> state);
+    turing -> state = NULL;
+  }
 
-  output = start_tm(tm1, band1);
-  printf("INP: %s\nOUT: %s\n\n", band1, output);
+  struct dnode* band_list = dlist_from_string(input_band);
+  turing -> state -> band = band_list;
 
-  output = start_tm(tm1, band2);
-  printf("INP: %s\nOUT: %s\n\n", band2, output);
-  
-  output = start_tm(tm1, band3);
-  printf("INP: %s\nOUT: %s\n\n", band3, output);
-  
-  //-----------------------------------------
-  printf("\nDECREMENT\n");
+  return TM_OKAY;
+}
 
-  output = start_tm(tm2, band1);
-  printf("INP: %s\nOUT: %s\n\n", band1, output);
-  
-  output = start_tm(tm2, band2);
-  printf("INP: %s\nOUT: %s\n\n", band2, output);
-  
-  output = start_tm(tm2, band3);
-  printf("INP: %s\nOUT: %s\n\n", band3, output);
 
-  //-----------------------------------------
-  printf("\nFUNCTION 2*X\n");
-  
-  output = start_tm(tm3, band1);
-  printf("INP: %s\nOUT: %s\n\n", band1, output);
-  
-  output = start_tm(tm3, band2);
-  printf("INP: %s\nOUT: %s\n\n", band2, output);
-  
-  output = start_tm(tm3, band3);
-  printf("INP: %s\nOUT: %s\n\n", band3, output);
-  //-----------------------------------------
-  
-  return 0;
+void tm_delete_machine(struct turing* turing) {
+  free(turing -> steps);
+  free(turing -> state);
+  free(turing);
 }
 
--- src/tm-sim.h
+++ src/tm-sim.h
@@ -1,9 +1,58 @@
 #ifndef TM_SIM
 
-#define TM_INIT_OKAY 0;
-#define TM_INIT_FAIL 1;
+#define TM_OKAY 0;
+#define TM_ACCEPT 2;
+#define TM_REJECT 3;
 
-#define TM_STATE_ACCEPT 2;
-#define TM_STATE_REJECT 3;
+// max size of a single state
+// e.g. "q0"
+#define STATE_SIZE 8
+
+// number of variables contained in a single step
+// e.g. "q0, 0 -> q0, 1, ->"
+#define VARS_IN_RULE 5
+
+// max size of single entry in step
+// e.g. "->"
+#define MAX_RULE_SIZE 16
+
+// max number of steps
+#define MAX_RULE_NUM 60
+
+
+// state of the tm
+struct state {
+  char  state [STATE_SIZE];
+  struct dnode*  band;
+  long  band_location;
+};
+
+// the turing machine
+struct turing {
+  short bands;
+  char  start  [STATE_SIZE];
+  char  accept [STATE_SIZE];
+  char  reject [STATE_SIZE];
+  char ***steps;
+  struct state* state;
+};
+
+// creates the turing machine
+struct turing* tm_create_machine(char tm[]);
+
+// sets the band for a given tm
+int tm_set_band(struct turing* turing, char input_band[]);
+
+// frees the allocated memory
+void tm_delete_machine(struct turing* turing);
+
+// runs the tm without stopping
+void run_tm(struct turing* turing);
+
+// runs the tm for a single step,
+// turing.state.band is the modified cell,
+// turing.state.state and itself before running is the used step
+// together with band and itself before running
+int run_tm_single_step(struct turing* turing);
 
 #endif // !TM_SIM
